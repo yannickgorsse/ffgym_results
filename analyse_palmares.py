3535a67be89712e41c005b43e8ca4d73c892d27a
Yannick GORSSE <yannick.gorsse@cea.fr>
2022-02-20 10:07:57 +0100

    improve legend

--- analyse_palmares.py
+++ analyse_palmares.py
@@ -4,28 +4,27 @@ import matplotlib.pyplot as plt
 import numpy as np
 from math import ceil
 import copy
-# import logging
+import logging
 
-# logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO, format="%(levelname)s:%(message)s")
 
 def get_data_from_json(my_js_file):
     with open(my_js_file, "r") as f:
         parsed = json.load(f)
     my_data = {}
     for event in parsed:
-        # titre : lieu - date_debut - date_fin
         lieu = event["event"]["lieu"]
         date_debut, date_fin = datetime.strptime(event["event"]["dateDebut"][:10], "%Y-%m-%d"), datetime.strptime(
             event["event"]["dateFin"][:10], "%Y-%m-%d"
         )
         date_debut, date_fin = date_debut.strftime("%d/%m/%Y"), date_fin.strftime("%d/%m/%Y")
         title = (lieu, f"{date_debut} - {date_fin}")
-        print(title)
+        logging.info(title)
         my_data[title] = {}
         for categorie in event["categories"]:
             all_gyms = {}
             cat = (categorie["label"], categorie["entityType"])
-            print(cat)
+            logging.info(cat)
             my_data[title][cat] = {}
             for team in categorie["teams"]:
                 city_team = (team["city"], team["label"])
@@ -33,15 +32,15 @@ def get_data_from_json(my_js_file):
                 for entity in team["entities"]:
                     if "mark" in entity:
                         my_data[title][cat][city_team]["gyms"][(entity["firstname"], entity["lastname"])] = {}
-                        # print(entity["firstname"], entity["mark"]["value"], entity["markRank"]) # markRank identique que l'equipe (en tout cas dans le cas equipe)
+                        logging.info(f"{entity['firstname']} {entity['mark']['value']} {entity['markRank']}") # markRank identique que l'equipe (en tout cas dans le cas equipe)
                         my_data[title][cat][city_team]["gyms"][(entity["firstname"], entity["lastname"])]["total"] = entity["mark"]["value"]
                         if (entity["firstname"], entity["lastname"]) in all_gyms:
                             raise Exception("pouet")
                         all_gyms[(entity["firstname"], entity["lastname"])] = float(entity["mark"]["value"])
                         for appm in entity["mark"]["appMarks"]:
-                            # print("    ", appm["labelApp"], appm["value"])
+                            logging.info(f"    {appm['labelApp']} {appm['value']}")
                             my_data[title][cat][city_team]["gyms"][(entity["firstname"], entity["lastname"])][appm["labelApp"]] = appm["value"]
-                # print(my_data[title])
+                logging.debug(my_data[title])
             dic_rank = {key: rank for rank, key in enumerate(sorted(all_gyms, key=all_gyms.get, reverse=True), 1)}
             for city, gyms in my_data[title][cat].items():
                 for nom_gym, _ in gyms["gyms"].items():
@@ -55,7 +54,7 @@ def filter_data_with(d, filter_str):
         for nc, cat in event.items():
             with_gif = False
             for (city, _), _ in cat.items():
-                if city == "GIF SUR YVETTE":
+                if city == filter_str:
                     with_gif = True
             if not with_gif: del filtered_dic[ne][nc]
 
@@ -65,7 +64,8 @@ def filter_data_with(d, filter_str):
 if __name__ == "__main__":
     for json_file in ["2022_01_23_morsang.json", "2022_02_13_bretigny.json"]:
         my_dic = get_data_from_json(json_file)
-        my_dic = filter_data_with(my_dic, "GIF SUR YVETTE")
+        gif = "GIF SUR YVETTE"
+        my_dic = filter_data_with(my_dic, gif)
         agres = ["Saut", "Barres asymétriques", "Poutre", "Sol"]
         agres = [*agres, agres[0]]
 
@@ -78,7 +78,7 @@ if __name__ == "__main__":
                 note_max = 0
                 teams = []
                 for (city, team), data_city in cat.items():
-                    if city == "GIF SUR YVETTE":
+                    if city == gif:
                         teams.append(team)
                 for (city, team), data_city in cat.items():
                     for nom_gym, notes in data_city["gyms"].items():
@@ -90,15 +90,15 @@ if __name__ == "__main__":
                         (axs[i // ny, i % ny] if nx > 1 else axs[i % ny]).plot(
                             label_loc,
                             marks,
-                            label=None if city != "GIF SUR YVETTE" else label,
-                            color="grey" if city != "GIF SUR YVETTE" else None,
-                            zorder=100 if city == "GIF SUR YVETTE" else 1,
-                            linewidth=2 if city == "GIF SUR YVETTE" else 0.75,
+                            label=None if city != gif else label,
+                            color="grey" if city != gif else None,
+                            zorder=100 if city == gif else 1,
+                            linewidth=2 if city == gif else 0.75,
                         )
                 t = f"{name_event} - {dates}\n{name_cat}" # - " + ("équipes" if entype == "EQU" else "indiv")
                 for team in teams:
-                    t_ = "\nClassement" + (team if len(teams) > 1 else "")
-                    t += (f"{t_} : {cat[('GIF SUR YVETTE', team)]['classement']}/{len(cat)}") if entype == "EQU" else ""
+                    t_ = "\nClassement " + (team if len(teams) > 1 else "")
+                    t += (f"{t_} : {cat[(gif, team)]['classement']}/{len(cat)}") if entype == "EQU" else ""
                 t += "\n"
                 (axs[i // ny, i % ny] if nx > 1 else axs[i % ny]).set_title(t, size=15)
                 (axs[i // ny, i % ny] if nx > 1 else axs[i % ny]).set_yticks(list(range(ceil(note_max))))
